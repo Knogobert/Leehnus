689519de3a825902d3fa4871ade74c40c0a02db4
NipSauce <SvenParker@wlanguest186136.eduroam.hj.se>
2015-09-28 15:03:31 +0200

    Checks if Master slider exists

--- part-main.php
+++ part-main.php
@@ -1,6 +1,6 @@
 <?php if( have_posts() ): while( have_posts() ): the_post(); ?>
 	<div>
-		<?php masterslider(1); ?>
+		<?php if ( function_exists( 'masterslider' ) ) masterslider(1); ?>
 	</div>
 	<article>
 		<h2><?php the_title(); ?></h2><!-- <?php the_permalink(); ?> -->
