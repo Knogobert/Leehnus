85d6b0c5d0a7fff1413e91b840ddf1fa2d543570
NipSauce <SvenParker@wlanguest186136.eduroam.hj.se>
2015-09-28 15:42:15 +0200

    Updated so if Master Slider doesn't exist, a backup-image is displayed

--- part-main.php
+++ part-main.php
@@ -1,9 +1,14 @@
 <?php if( have_posts() ): while( have_posts() ): the_post(); ?>
-	<div>
-		<?php if ( function_exists( 'masterslider' ) ) masterslider(1); ?>
-	</div>
+	<figure>
+		<?php if ( function_exists( 'masterslider' ) ) : ?>
+			<?php masterslider(1); ?>
+		<?php else : ?>
+			<img src="<?php echo get_template_directory_uri(); ?>/Planning/Leehnus%20-%20Class%20diagram%202.2.png"/><!-- Put in a standard picture 960x360 here -->
+		<?php endif; ?>
+	</figure>
 	<article>
 		<h2><?php the_title(); ?></h2><!-- <?php the_permalink(); ?> -->
+		<?php get_the_post_thumbnail ( 'infoPhoto', 'thumbnail' ) ?>
 		<p><?php the_excerpt(); ?></p>
 	</article>
 <?php endwhile; endif; ?>
