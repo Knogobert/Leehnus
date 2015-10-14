217f21f8ba50756173e2e36ed1198fc10d36d000
Sven Parker <SvenParker@MacMuffin.local>
2015-10-14 16:12:15 +0200

    First draft on Gallery and Contact pages. Custom post type: contacts

--- functions.php
+++ functions.php
@@ -51,7 +51,7 @@ function leehnus_add_theme_support() {
 		'uploads'       => true,
 	));
 	add_theme_support('post-thumbnails');
-	add_theme_support('post-formats', array('aside', 'video'));
+	add_theme_support('post-formats', array('aside', 'video', 'gallery'));
 
 	register_nav_menus(array(
 		'main_menu' => 'Main menu',
@@ -64,41 +64,17 @@ add_action('init', 'leehnus_add_theme_support');
 
 /* Our custom post types */
 function leehnus_post_types() {
-	register_post_type( 'person', array(
+	register_post_type( 'contact', array(
 		'public' => true,
 		'labels' => array(
-			'name' => 'Persons' ),
+			'name' => 'Leehnus Contacts',
+			'singular_name'      => 'Leehnus Contact' ),
 		'hierarchical' => false,
 		'supports' => array(
-			'title', 'editor', 'author'
+			'title', 'editor', 'thumbnail', 'author'
 		)
 	) );
 }
 add_action('init', 'leehnus_post_types');
-
-
-// ____________________________WOOCOMMERCE___________________________
-
-// Declares WC support
-function woocommerce_support() {
-    add_theme_support( 'woocommerce' );
-}
-add_action( 'after_setup_theme', 'woocommerce_support' );
-
-// Unhook WC wrappers
-remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
-remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
-
-// Hook leehnus wrappers
-add_action('woocommerce_before_main_content', 'leehnus_wrapper_start', 10);
-add_action('woocommerce_after_main_content', 'leehnus_wrapper_end', 10);
-
-function leehnus_wrapper_start() {
-  echo '<main id="main">';
-}
-
-function leehnus_wrapper_end() {
-  echo '</main>';
-}
 
 ?>
--- page-contact.php
+++ page-contact.php
@@ -2,6 +2,26 @@
 	<main id="main" class="site-main" role="main">
 	
 		<h1>This is page-contact.php</h1>
-
+		
+		<?php
+		    $contacts = new WP_Query( array('post_type' => 'contact') );
+		    if( $contacts->have_posts() ) {
+		      while( $contacts->have_posts() ) {
+		        $contacts->the_post();
+		        ?>
+		          <h1><?php the_title() ?></h1>
+		          <div class="contactThumb">
+			          <?php the_post_thumbnail () ?>
+		          </div>
+		          <div class='content'>
+		            <?php the_content() ?>
+		          </div>
+		        <?php
+		      }
+		    }
+		    else {
+		      echo 'There are no contact(s) to display. Create a "Leehnus Contact" in the wordpress admin side-menu';
+		    }
+		  ?>
 	</main><!-- #main -->
 <?php get_footer() ?>
