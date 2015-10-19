d52ab5bfbc3b116e3c9c2701533131ff324c26d0
Sven Parker <SvenParker@MacMuffin.local>
2015-10-19 21:34:37 +0200

    Added overriding template files for woocommerce

---
+++ woocommerce/content-single-product.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * The template for displaying product content in the single-product.php template
+ *
+ * Override this template by copying it to yourtheme/woocommerce/content-single-product.php
+ *
+ * @author 		WooThemes
+ * @package 	WooCommerce/Templates
+ * @version     1.6.4
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+?>
+
+<?php
+	/**
+	 * woocommerce_before_single_product hook
+	 *
+	 * @hooked wc_print_notices - 10
+	 */
+	 do_action( 'woocommerce_before_single_product' );
+
+	 if ( post_password_required() ) {
+	 	echo get_the_password_form();
+	 	return;
+	 }
+?>
+
+<div itemscope itemtype="<?php echo woocommerce_get_product_schema(); ?>" id="product-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+	<?php
+		/**
+		 * woocommerce_before_single_product_summary hook
+		 *
+		 * @hooked woocommerce_show_product_sale_flash - 10
+		 * @hooked woocommerce_show_product_images - 20
+		 */
+		do_action( 'woocommerce_before_single_product_summary' );
+	?>
+
+	<div class="summary entry-summary">
+
+		<?php
+			/**
+			 * woocommerce_single_product_summary hook
+			 *
+			 * @hooked woocommerce_template_single_title - 5
+			 * @hooked woocommerce_template_single_rating - 10
+			 * @hooked woocommerce_template_single_price - 10
+			 * @hooked woocommerce_template_single_excerpt - 20
+			 * @hooked woocommerce_template_single_add_to_cart - 30
+			 * @hooked woocommerce_template_single_meta - 40
+			 * @hooked woocommerce_template_single_sharing - 50
+			 */
+			do_action( 'woocommerce_single_product_summary' );
+		?>
+
+	</div><!-- .summary -->
+
+	<?php
+		/**
+		 * woocommerce_after_single_product_summary hook
+		 *
+		 * @hooked woocommerce_output_product_data_tabs - 10
+		 * @hooked woocommerce_upsell_display - 15
+		 * @hooked woocommerce_output_related_products - 20
+		 */
+		do_action( 'woocommerce_after_single_product_summary' );
+		
+		/* CUSTOM
+			function action_woocommerce_after_single_product_summary( $woocommerce_output_product_data_tabs, $number ) {
+			    // make action magic happen here...
+			};
+			add_action( 'woocommerce_after_single_product_summary', 'action_woocommerce_after_single_product_summary', 10, 2 );
+		*/
+	?>
+	
+	<!-- CUSTOM -->
+	<!--
+<?php $tabs = apply_filters( 'woocommerce_product_tabs', array() );
+
+	if ( ! empty( $tabs ) ) : ?>
+		<ul class="tabs wc-tabs">
+			<?php foreach ( $tabs as $key => $tab ) : ?>
+				<li class="<?php echo esc_attr( $key ); ?>_tab">
+					<a href="#tab-<?php echo esc_attr( $key ); ?>"><?php echo apply_filters( 'woocommerce_product_' . $key . '_tab_title', esc_html( $tab['title'] ), $key ); ?></a>
+				</li>
+			<?php endforeach; ?>
+		</ul>
+	<?php endif; ?>
+-->
+
+	<meta itemprop="url" content="<?php the_permalink(); ?>" />
+
+</div><!-- #product-<?php the_ID(); ?> -->
+
+<?php do_action( 'woocommerce_after_single_product' ); ?>
