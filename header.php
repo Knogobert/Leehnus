d9e6e5b6bf2dbe6023846a828938ee20f4da8383
JesperSunden <jespersunden@telia.com>
2015-10-21 15:27:30 +0200

    Fixed header

--- header.php
+++ header.php
@@ -7,16 +7,16 @@
 	<?php wp_head(); ?>
 </head>
 <body>
-	<div class="page-wrapper">
-		<header>
-			<center>
-				<a href="<?php bloginfo('url'); ?>">
-					<img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="Home" />
-				</a>
-			</center>
-			<?php wp_nav_menu( array(
-				'menu' => 'main menu',
-				'theme_location' => 'main_menu',
-				'container' => 'nav'
+	<header>
+		<center>
+			<a href="<?php bloginfo('url'); ?>">
+				<img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="Home" />
+			</a>
+		</center>
+		<?php wp_nav_menu( array(
+			'menu' => 'main menu',
+			'theme_location' => 'main_menu',
+			'container' => 'nav'
 			) ); ?>
-		</header>
+	</header>
+	<div class="page-wrapper">
