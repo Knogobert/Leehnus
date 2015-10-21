ae689d5e1dd372720895ebefc6c13cc5c39005e7
JesperSunden <jespersunden@telia.com>
2015-10-21 15:13:32 +0200

    Made 404.php

---
+++ 404.php
@@ -0,0 +1,6 @@
+<?php get_header() ?>
+    <main id="main" class="site-main" role="main">
+        <h2>ERROR 404 PAGE NOT FOUND, SUCKS TO BE YOU!</h2>
+    </main><!-- #main -->
+    <!-- ../wp-content/themes/Leehnus/img/5.jpg -->
+<?php get_footer() ?>
